707b9aa241babd645549c3e79451062257f920b1
repro-tetris <77537560+repro-tetris@users.noreply.github.com>
2021-11-22 23:06:44 +0200

    refactoring

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const controllerWrapper = require("../../middlewares/controllerWrapper");
-const getAll = require("../../controllers/contacts/getAll");
-const getById = require("../../controllers/contacts/getById");
-const deleteById = require("../../controllers/contacts/deleteById");
-const add = require("../../controllers/contacts/add");
-const updateById = require("../../controllers/contacts/updateById");
-const contactSchema = require("../../schemas/contact");
+const contactController = require("../../controllers/contacts");
+const contactSchema = require("../../schemas/contactSchema");
 const validation = require("../../middlewares/validation");
 
-router.get("/", controllerWrapper(getAll));
+router.get("/", controllerWrapper(contactController.getAll));
 
-router.get("/:contactId", controllerWrapper(getById));
+router.get("/:contactId", controllerWrapper(contactController.getById));
 
-router.post("/", validation(contactSchema), controllerWrapper(add));
+router.post(
+  "/",
+  validation(contactSchema),
+  controllerWrapper(contactController.add)
+);
 
-router.delete("/:contactId", controllerWrapper(deleteById));
+router.delete("/:contactId", controllerWrapper(contactController.deleteById));
 
 router.put(
   "/:contactId",
   validation(contactSchema),
-  controllerWrapper(updateById)
+  controllerWrapper(contactController.updateById)
 );
 
 module.exports = router;
